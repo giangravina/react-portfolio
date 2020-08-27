11d82db145c469173bca90710bc71fe982f8f321
Gian <giangravina@gmail.com>
2020-08-27 15:16:26 -0400

    added about page, contact page, and homepage + routes

---
+++ src/pages/ContactPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function ContactPage(props) {
+
+    return (
+        <p>Contact Page works!</p>
+    );
+}
+
+export default ContactPage;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function HomePage(props) {
+
+    return (
+        <p>homepage works!</p>
+    );
+}
+
+export default HomePage;
